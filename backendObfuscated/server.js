eb5016e0110cb95553f392c7f88e4ab46eeb84af
Teja Dasari <129512442+DasariTeja09@users.noreply.github.com>
2025-06-17 23:07:28 +0530

    Update server.js

--- backendObfuscated/server.js
+++ backendObfuscated/server.js
@@ -17,3 +17,6 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`🚀 Server running on port ${port}`);
 });
+app.get('/', (req, res) => {
+  res.send('Homepage Loaded!');
+});
